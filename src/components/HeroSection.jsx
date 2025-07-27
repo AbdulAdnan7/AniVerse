596b5391c6bd1c59b6c179ef5faf3337f4a67352
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-27 16:37:02 +0530

    Added API

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,32 +5,32 @@ import LOM from '../assets/LOM.jpg';
 
 const HeroSection = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [animeList, setAnimeList] = useState([])
 
-  const animeList = [
-    {
-      title: 'Attack on Titan',
-      img: AOT,
-      alt: 'Attack on Titan',
-      desc: 'Humanity lives within three concentric walls for a century, protected from giant, man-eating beings called Titans.',
-      objectPosition: 'object-center'
-    },
-    {
-      title: 'Demon Slayer',
-      img: DS,
-      alt: 'Demon Slayer',
-      desc: 'The story takes place in a world where demons, transformed humans with supernatural abilities, prey on humans.',
-      objectPosition: 'object-center'
-    },
-    {
-      title: 'Lord Of Mystries',
-      img: LOM,
-      alt: 'Lord Of Mystries',
-      desc: 'Humans in this world can gain supernatural abilities by consuming Beyonder potions and following specific pathways, becoming "Beyonders".',
-      objectPosition: 'object-center'
-    }
-  ];
-
+  
+  //For Slides and Buttons To Change Index
   const anime = animeList[currentIndex];
+
+  useEffect(() => {
+    //Fetching the api
+    const fetchTopAnime = async () => {
+      try {
+        const res = await fetch('https://api.jikan.moe/v4/top/anime?limit=10');
+        const data = await res.json();
+        const topAnime = data.data.map((anime) => ({
+          title: anime.title,
+          img: anime.images.jpg.large_image_url,
+          alt: anime.title,
+          desc: anime.synopsis,
+          objectPosition: 'object-center'
+        }))
+        setAnimeList(topAnime)
+      } catch (err) {
+        console.log('Failed to fetch top anime: ', err)
+      }
+    }
+    fetchTopAnime()
+  }, [])
 
   const prevSlide = () => {
     setCurrentIndex((prev) => (prev - 1 + animeList.length) % animeList.length);
@@ -43,9 +43,11 @@ const HeroSection = () => {
    useEffect(() => {
      const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % animeList.length)
-     }, 3000)
+     }, 4000)
      return () => clearInterval(interval)
-   }, []) 
+   }, [animeList]);
+   
+   if (animeList.length === 0) return <div className='text-white p-10'>Loading....</div>
 
   return (
     <div>
@@ -58,7 +60,7 @@ const HeroSection = () => {
 
         <div className='absolute bottom-4 left-4 sm:left-8 pr-12 text-white z-10'>
           <h1 className='text-5xl font-semibold'>{anime.title}</h1>
-          <p className='font-semibold max-w-lg mt-2'>{anime.desc}</p>
+          <p className='font-semibold max-w-lg mt-2 truncate'>{anime.desc}</p>
           <div className='mt-4 flex gap-4'>
             <button className='bg-[#6C5CE7] hover:bg-[#5A4BCF] text-white p-2 px-4 rounded'>Watch now</button>
             <button className='bg-transparent border text-white border-white px-4 py-2 rounded hover:bg-white hover:text-black transition'>Detail</button>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,21 +10,6 @@ const Home = () => {
     <HeroSection />
    </section>
   )
-  {
-    /**
-     * <div className='absolute inset-0 left-[400px] right-20 top-58'>
-  <div className='flex gap-4'>
-    <button className='bg-gra p-2 rounded-full'>
-      <FontAwesomeIcon icon={faAngleLeft} />
-    </button>
-    <button className='bg-gray-400 p-2 rounded-full'>
-      <FontAwesomeIcon icon={faAngleRight} />
-    </button>
-  </div>
-</div>
-
-     */
-  }
 }
 
 export default Home
