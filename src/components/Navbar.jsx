7f784ad1ca295d2822d2344597602ebe62a52481
Abdul Adnan <peerabduladnan@gmail.com>
2025-07-29 15:02:11 +0530

    Added Search Functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import SearchBar from './SearchBar'
 
 const Navbar = () => {
   return (
@@ -8,16 +9,7 @@ const Navbar = () => {
       <header className='font-bold px-1 py-1'>
        <h1 className='text-white'>ANI<span className='text-yellow-400'>VERSE.</span></h1>
       </header>
-      <div className="flex items-center gap-2">
-  <input
-    type="text"
-    placeholder="Search anime..."
-    className="px-3 py-1.5 rounded-md bg-[#1a1a2e] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500 transition-all"
-  />
-  <button className="px-3 py-1.5 bg-pink-500 hover:bg-pink-600 text-white rounded-md transition-all">
-    Search
-  </button>
-</div>
+      <SearchBar />
 
     </nav>
     </>
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const SearchBar = () => {
+  const [search, setSearch] = useState('')
+  const [result, setResult] = useState([])
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const fetchAnime = async () => {
+      if(search.length < 2) return setResult([]);
+
+      try {
+        const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=5`)
+        const data = await res.json();
+        setResult(data.data || [])
+      } catch (err) {
+        console.error('Error Fetching anime: ', err)
+      }
+    }
+
+    fetchAnime()
+  }, [search])
+
+  const handleSelection = (anime) => {
+    setSearch(anime.title)
+    setResult([]);
+    navigate(`/anime/${anime.mal_id}`)
+  }
+
+
   return (
-    <div>
-      
-    </div>
+  <div className="relative w-full max-w-md mx-auto">
+ <input
+  type="text"
+  value={search}
+  onChange={(e) => setSearch(e.target.value)}
+  placeholder="Search anime..."
+  className="w-full px-3 py-2 rounded-md bg-[#1a1a2e] text-white placeholder-gray-400 focus:outline-none focus:ring-2 focus:ring-pink-500 transition"
+/>
+  {result.length > 0 && (
+    <ul className="absolute mt-1 w-full bg-[#1a1a2e] text-white rounded shadow-lg z-50 max-h-60 overflow-y-auto divide-y divide-gray-700">
+          {result.map((anime) => (
+           <li
+  key={anime.mal_id}
+  onClick={() => handleSelection(anime)}
+  className="px-4 py-2 hover:bg-pink-600 cursor-pointer transition-all text-sm"
+>
+
+              {anime.title}
+            </li>
+          ))}
+        </ul>
+      )}
+</div>
   )
 }
 
